dec96e63f1df38c5cdd81d04d524485f4689bba6
Alexander Bischof <alexanderxbischof@gmail.com>
2015-10-16 23:53:12 +0200

    Refactoring for config.properties

--- src/main/java/de/bischinger/parrot/gui/Main.java
+++ src/main/java/de/bischinger/parrot/gui/Main.java
@@ -1,5 +1,9 @@
 package de.bischinger.parrot.gui;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+
 import static java.lang.Integer.valueOf;
 
 public class Main {
@@ -8,29 +12,38 @@ public class Main {
 
         String ip = "192.168.2.1";
         int port = 44444;
-        String sumoWlan = "JumpingSumo-b169798";
+        String wlan = "JumpingSumo-b169798";
+
+        File configFile = new File("config.properties");
+        if (configFile.exists()){
+            Properties properties = new Properties();
+            properties.load(new FileReader(configFile));
+            ip = properties.getProperty("ip");
+            port = Integer.valueOf(properties.getProperty("port"));
+            wlan = properties.getProperty("wlan");
+        }
 
         if (args.length > 0) {
             switch (args[0].toLowerCase()) {
                 case "keyboard":
                     int speedConfig = args.length > 1 ? valueOf(args[1]) : 100;
                     int turnConfig = args.length > 2 ? valueOf(args[2]) : 30;
-                    new KeyboardDriver(ip, port, sumoWlan, speedConfig, turnConfig);
+                    new KeyboardDriver(ip, port, wlan, speedConfig, turnConfig);
                     break;
                 case "program":
-                    new ProgrammaticDriver(ip, port, sumoWlan).drive();
+                    new ProgrammaticDriver(ip, port, wlan).drive();
                     break;
                 case "file":
-                    new FileBasedProgrammaticDriver(ip, port, sumoWlan);
+                    new FileBasedProgrammaticDriver(ip, port, wlan);
                     break;
                 case "swing":
-                    new SwingBasedProgrammaticDriver(ip, port, sumoWlan);
+                    new SwingBasedProgrammaticDriver(ip, port, wlan);
                     break;
                 default:
                     System.out.println("Argument unbekannt: keyboard | program | file | swing");
             }
         } else {
-            new KeyboardDriver(ip, port, sumoWlan, 100, 30);
+            new KeyboardDriver(ip, port, wlan, 100, 30);
         }
     }
 }
